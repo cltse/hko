5073fed6e0e6689ee8aa16ba155796cf6c858e0e
cltse <55041167+cltse@users.noreply.github.com>
2023-03-19 12:35:15 +0800

    added useful links

--- src/App.js
+++ src/App.js
@@ -11,24 +11,28 @@ const imageData = [
   {
     title: '太平山(望向東北偏北面)',
     src: 'https://www.hko.gov.hk/wxinfo/aws/hko_mica/vpb/latest_HD_VPB.jpg',
+    url: 'https://www.hko.gov.hk/tc/wxinfo/ts/webcam/VPB_photo.htm',
   },
   {
     title: '太平山(望向東面)',
     src: 'https://www.hko.gov.hk/wxinfo/aws/hko_mica/vpa/latest_HD_VPA.jpg',
+    url: 'https://www.hko.gov.hk/tc/wxinfo/ts/webcam/VPA_photo.htm',
   },
   {
     title: '環球貿易廣場(望向西南面)',
     src: 'https://www.hko.gov.hk/wxinfo/aws/hko_mica/ic2/latest_HD_IC2.jpg',
+    url: 'https://www.hko.gov.hk/tc/wxinfo/ts/webcam/IC2_photo.htm',
   },
   {
     title: '環球貿易廣場(望向東南面)',
     src: 'https://www.hko.gov.hk/wxinfo/aws/hko_mica/ic1/latest_HD_IC1.jpg',
+    url: 'https://www.hko.gov.hk/tc/wxinfo/ts/webcam/IC1_photo.htm',
   },
 ];
 
-const ImageCol = ({ title, src }) => (
+const ImageCol = ({ title, src, url }) => (
   <div>
-    <h4>{title}</h4>
+    <h4><a href={url} target='_blank'>{title}</a></h4>
     <Image fluid src={src} />
   </div>
 );
@@ -86,6 +90,19 @@ const App = () => {
 
   const isTwoByTwo = isMobile ? false : !isSmallDesktop;
 
+  function getHKOWebPage() {
+    return (
+      <div>
+      <div>
+        <a href="https://www.hko.gov.hk/tc/wxinfo/ts/index_webcam.htm" target={'_blank'}>最新天氣照片</a>
+      </div>
+      <div>
+        <a href="https://www.hko.gov.hk/tc/out_photo/outdoor_photo.htm" target={'_blank'}>戶外攝影天氣資訊網頁</a>
+      </div>
+      </div>
+    )
+  }
+
   function getOutputContainer(isTwoByTwo) {
     if(isTwoByTwo) {
       // console.log("two by two");
@@ -94,14 +111,14 @@ const App = () => {
         <Row>
           <Col>
             <Row>
-              <ImageCol title={imageData[0].title} src={imageData[0].src} />
-              <ImageCol title={imageData[1].title} src={imageData[1].src} />
+              <ImageCol title={imageData[0].title} src={imageData[0].src} url={imageData[0].url} />
+              <ImageCol title={imageData[1].title} src={imageData[1].src} url={imageData[1].url} />
             </Row>
           </Col>
           <Col>
             <Row>
-              <ImageCol title={imageData[2].title} src={imageData[2].src} />
-              <ImageCol title={imageData[3].title} src={imageData[3].src} />
+              <ImageCol title={imageData[2].title} src={imageData[2].src} url={imageData[2].url} />
+              <ImageCol title={imageData[3].title} src={imageData[3].src} url={imageData[3].url} />
             </Row>
           </Col>
         </Row>
@@ -111,16 +128,16 @@ const App = () => {
     else {
       // console.log("four by one");
       return (
-      <Container fluid className="Container">
-      <Row>
-        <Col>
-          <ImageCol title={imageData[0].title} src={imageData[0].src} />
-          <ImageCol title={imageData[1].title} src={imageData[1].src} />
-          <ImageCol title={imageData[2].title} src={imageData[2].src} />
-          <ImageCol title={imageData[3].title} src={imageData[3].src} />
-        </Col>
-      </Row>
-    </Container>
+        <Container fluid className="Container">
+        <Row>
+          <Col>
+            <ImageCol title={imageData[0].title} src={imageData[0].src} />
+            <ImageCol title={imageData[1].title} src={imageData[1].src} />
+            <ImageCol title={imageData[2].title} src={imageData[2].src} />
+            <ImageCol title={imageData[3].title} src={imageData[3].src} />
+          </Col>
+        </Row>
+      </Container>
       )
     }
   }
@@ -130,6 +147,7 @@ const App = () => {
       <div className="App">
         <header className="App-header">
           {getOutputContainer(isTwoByTwo)}
+          {/* {getHKOWebPage()} */}
         </header>
       </div>
     </div>
